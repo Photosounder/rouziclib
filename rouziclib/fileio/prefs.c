9d7a217ea60f9a872e762612f91c0895cb1cb2e0
Photosounder <contact@photosounder.com>
2019-08-06 14:07:50 +0200

    Preference setting and threaded saving

--- rouziclib/fileio/prefs.c
+++ rouziclib/fileio/prefs.c
@@ -16,6 +16,32 @@ int pref_file_save(pref_file_t *pf)
 	return save_string_array_to_file(pf->path, "wb", pf->lines, pf->linecount);
 }
 
+int pref_file_save_thread(pref_file_t *pf)
+{
+	int ret;
+
+	ret = pref_file_save(pf);
+	free_pref_file(pf);
+	free(pf);
+
+	return ret;
+}
+
+void pref_file_save_thread_launch(pref_file_t *pf)
+{
+	pref_file_t *pf_copy;
+
+	pf_copy = calloc(1, sizeof(pref_file_t));
+
+	// Copy the data
+	pf_copy->linecount = pf->linecount;
+	pf_copy->lines = make_string_array_copy(pf->lines, pf->linecount);
+	pf_copy->path = make_string_copy(pf->path);
+
+	// Launch the thread
+	rl_thread_create_detached(pref_file_save_thread, pf_copy);
+}
+
 void free_pref_file(pref_file_t *pf)
 {
 	free(pf->path);
@@ -50,13 +76,9 @@ int pref_find_loc_depth(pref_file_t *pf, const char *loc, int depth, int start, 
 	char key[64];
 
 	// Find the key at the given depth
-	ret = string_get_field(loc, ":", depth, &key[depth]);
-	if (ret==0)
-		return -1;
-
-	// Indent the key
-	for (i=0; i < depth; i++)
-		key[i] = '\t';
+	memset(key, '\t', depth);					// indent the key
+	if (string_get_field(loc, ":", depth, &key[depth]) == 0)
+		return -1;						// if loc isn't that deep
 
 	// Find where the key is declared (start)
 	for (i=start; i < end; i++)
@@ -119,38 +141,50 @@ int pref_find_loc(pref_file_t *pf, const char *loc)
 // v = pref_get_double(&pref_def, "Audio output:Sample rate", 44100, "Hz");
 // Audio output
 // 	Sample rate: 44100 Hz
-double pref_get_double(pref_file_t *pf, char *loc, double def_value, const char *suffix)
+double pref_handle_double(pref_file_t *pf, char *loc, double value, const char *suffix, int mode_set)
 {
 	int i, line_pos, depth;
 	char key[64], *new_line;
-	double input_v=NAN;
+	double read_value=NAN;
 
 	// Find the line for loc
 	line_pos = pref_find_loc(pf, loc);
 	if (line_pos < 0)
 		return NAN;
 
-	depth = string_count_fields(loc, ":") - 1;
+	// Make key string
+	depth = string_count_fields(loc, ":") - 1;			// find the indentation depth
+	memset(key, '\t', depth);					// indent the key
 	if (string_get_field(loc, ":", depth, &key[depth]) == 0)	// get the key in the last field
 		return NAN;
-
-	// Indent the key
-	for (i=0; i < depth; i++)
-		key[i] = '\t';
 
 	// Try to read the value in the line
 	char *p = strstr_after(pf->lines[line_pos], key);		// find what's after the key in the key's line
 	if (p)
-		if (sscanf(p, ": %lg", &input_v) == 1)			// if the value is found
-			return input_v;
+		if (sscanf(p, ": %lg", &read_value) == 1)		// if the value is found
+			if (mode_set==0 || read_value == value)		// return if get mode or if it's the same as the one written
+				return read_value;
 
 	// Recreate the line with the default value if the value couldn't be read because only the key is there yet
 	if (suffix)
-		new_line = sprintf_realloc(NULL, NULL, 0, "%s: %g %s", key, def_value, suffix);
+		new_line = sprintf_realloc(NULL, NULL, 0, "%s: %g %s", key, value, suffix);
 	else
-		new_line = sprintf_realloc(NULL, NULL, 0, "%s: %g", key, def_value);
+		new_line = sprintf_realloc(NULL, NULL, 0, "%s: %g", key, value);
 	pf->lines = string_array_replace_line(pf->lines, &pf->linecount, new_line, line_pos);
 	free(new_line);
 
-	return def_value;
+	if (mode_set)
+		pref_file_save_thread_launch(pf);
+
+	return value;
+}
+
+double pref_get_double(pref_file_t *pf, char *loc, double def_value, const char *suffix)
+{
+	return pref_handle_double(pf, loc, def_value, suffix, 0);
+}
+
+void pref_set_double(pref_file_t *pf, char *loc, double new_value, const char *suffix)
+{
+	pref_handle_double(pf, loc, new_value, suffix, 1);
 }
--- rouziclib/fileio/prefs.h
+++ rouziclib/fileio/prefs.h
@@ -12,6 +12,6 @@ extern int pref_file_save(pref_file_t *pf);
 extern void free_pref_file(pref_file_t *pf);
 extern pref_file_t pref_set_file_by_path(const char *path);
 extern pref_file_t pref_set_file_by_appdata_path(const char *folder);
-extern int pref_find_loc_depth(pref_file_t *pf, const char *loc, int depth, int start, int end);
 extern int pref_find_loc(pref_file_t *pf, const char *loc);
 extern double pref_get_double(pref_file_t *pf, char *loc, double def_value, const char *suffix);
+extern void pref_set_double(pref_file_t *pf, char *loc, double new_value, const char *suffix);
