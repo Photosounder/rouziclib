7c90abc182e8628f35a0f39e6009035d19503814
Photosounder <contact@photosounder.com>
2024-06-03 22:48:50 +0200

    Removed old Spiral-only functions

--- rouziclib/graphics/drawprimitives.h
+++ rouziclib/graphics/drawprimitives.h
@@ -14,8 +14,6 @@ extern void draw_line_dashed(xy_t p1, xy_t p2, double dash_period, double dash_r
 extern int32_t get_dist_to_roundrect(int32_t lx1, int32_t ly1, int32_t lx2, int32_t ly2, int32_t corner, int32_t ixf, int32_t iyf);
 extern void draw_roundrect(rect_t box, double corner, double radius, lrgb_t colour, const blend_func_t bf, double intensity);
 extern void draw_roundrect_frame(rect_t box1, rect_t box2, double corner1, double corner2, double radius, lrgb_t colour, const blend_func_t bf, double intensity);
-extern void draw_polar_glow(double cx, double cy, lrgb_t col, double colmul, double scale, double rad, double gradr, double gradth, double angle, int32_t islog, int32_t riserf, double erfrad, double pixoffset);
-extern void draw_gaussian_gradient(double cx, double cy, lrgb_t c0, lrgb_t c1, double gausrad, double gausoffx, double gausoffy, const blend_func_t bf);
 extern void draw_point_lrgb(xy_t pos, double radius, lrgb_t colour, const blend_func_t bf, double intensity);
 extern void draw_point_frgb(xy_t pos, double radius, frgb_t colour, const blend_func_fl_t bf, double intensity);
 extern void draw_point_dq(xy_t pos, double radius, frgb_t colour, double intensity);
