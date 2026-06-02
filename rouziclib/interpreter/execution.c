b2684c51ed7cf301b1a4737d48bfec56f02fef22
Photosounder <contact@photosounder.com>
2026-06-02 03:01:06 +0200

    Slight rlip_execute_opcode() optimisations

--- rouziclib/interpreter/execution.c
+++ rouziclib/interpreter/execution.c
@@ -1,32 +1,47 @@
+#ifndef RLIP_KILLSWITCH_POLL_INTERVAL
+#define RLIP_KILLSWITCH_POLL_INTERVAL 64
+#endif
+
 int rlip_execute_opcode(rlip_t *d)
 {
 	opint_t *op = d->op, *op_next;
 	double *vd = d->vd;
 	int64_t *vi = d->vi;
 	uint8_t *vr = d->vr;
+	void **ptr = d->ptr;
+	double *return_value = d->return_value;
+	uint8_t *return_real = d->return_real;
+	rlip_real_functions_t *rf = &d->rf;
+	volatile int *exec_on = d->exec_on;
 	int i;
 
 	if (d->valid_prog == 0)
 	{
-		if (d->return_value)
-			d->return_value[0] = NAN;
+		if (return_value)
+			return_value[0] = NAN;
 		return -1;
 	}
 
-	while (*d->exec_on)
+	if (*exec_on == 0)
+		return 0;
+
+	int loop_count = RLIP_KILLSWITCH_POLL_INTERVAL;
+	while (1)
 	{
+		int code = op[0];
+
 		// Point to next op
-		op_next = &op[op[0] >> 10];
+		op_next = &op[code >> 10];
 
 		// Execute op
-		switch (op[0])
+		switch (code)
 		{
 			// 1 word ops
 			case op_end:	return 1;
 
 			// 2 word ops
-			break;	case op_ret1_d:		d->return_value[0] = vd[op[1]];					// return <var/ptr/expr>
-			break;	case op_ret1_r:		d->rf.set(&d->return_real[0], &vr[op[1]]);
+			break;	case op_ret1_d:		return_value[0] = vd[op[1]];					// return <var/ptr/expr>
+			break;	case op_ret1_r:		rf->set(&return_real[0], &vr[op[1]]);
 			break;	case op_jmp:		op_next = &op[(ptrdiff_t) op[1]];				// unused for now
 			break;	case op_set0_d:		vd[op[1]] = 0.;							// set0 <var>
 			break;	case op_set0_i:		vi[op[1]] = 0;
@@ -34,31 +49,31 @@ int rlip_execute_opcode(rlip_t *d)
 			break;	case op_inc1_i:		vi[op[1]] += 1;
 
 			// 3 word ops
-			break;	case op_ret2_d:		for (i=0; i<2; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> <var/ptr/expr>
-			break;	case op_ret2_r:		for (i=0; i<2; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
-			break;	case op_load_d:		vd[op[1]] = *(double *) d->ptr[op[2]];				// compiler-only
-			break;	case op_load_i:		vi[op[1]] = *(int64_t *) d->ptr[op[2]];
-			break;	case op_load_r:		d->rf.set(&vr[op[1]], d->ptr[op[2]]);
+			break;	case op_ret2_d:		for (i=0; i<2; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> <var/ptr/expr>
+			break;	case op_ret2_r:		for (i=0; i<2; i++) rf->set(&return_real[i], &vr[op[i+1]]);
+			break;	case op_load_d:		vd[op[1]] = *(double *) ptr[op[2]];				// compiler-only
+			break;	case op_load_i:		vi[op[1]] = *(int64_t *) ptr[op[2]];
+			break;	case op_load_r:		rf->set(&vr[op[1]], ptr[op[2]]);
 			break;	case op_set_d:		vd[op[1]] = vd[op[2]];						// <var> = <var/ptr/expr>
 			break;	case op_set_i:		vi[op[1]] = vi[op[2]];
-			break;	case op_set_r:		d->rf.set(&vr[op[1]], &vr[op[2]]);
+			break;	case op_set_r:		rf->set(&vr[op[1]], &vr[op[2]]);
 			break;	case op_cvt_i_d:	vd[op[1]] = (double) vi[op[2]];					// <var> = <var/ptr/expr>
 			break;	case op_cvt_d_i:	vi[op[1]] = (int64_t) vd[op[2]];
-			break;	case op_cvt_r_d:	vd[op[1]] = d->rf.cvt_r_d(&vr[op[2]]);
-			break;	case op_cvt_d_r:	d->rf.cvt_d_r(&vr[op[1]], vd[op[2]]);
-			break;	case op_cvt_r_i:	vi[op[1]] = d->rf.cvt_r_i(&vr[op[2]]);
-			break;	case op_cvt_i_r:	d->rf.cvt_i_r(&vr[op[1]], vi[op[2]]);
+			break;	case op_cvt_r_d:	vd[op[1]] = rf->cvt_r_d(&vr[op[2]]);
+			break;	case op_cvt_d_r:	rf->cvt_d_r(&vr[op[1]], vd[op[2]]);
+			break;	case op_cvt_r_i:	vi[op[1]] = rf->cvt_r_i(&vr[op[2]]);
+			break;	case op_cvt_i_r:	rf->cvt_i_r(&vr[op[1]], vi[op[2]]);
 
 			break;	case op_sq_d:		vd[op[1]] = sq(vd[op[2]]);					// <var> = sq <var/ptr/expr>
 			break;	case op_sqrt_d:		vd[op[1]] = sqrt(vd[op[2]]);					// <var> = sqrt <var/ptr/expr>
 
 			break;	case op_jmp_cond:	if (vi[op[1]]) { op_next = &op[(ptrdiff_t) op[2]]; }		// if <var> goto <loc>
-			break;	case op_func0_d:	vd[op[1]] = ((double (*)(void)) d->ptr[op[2]])();
-			break;	case op_func0_r:	((void (*)(uint8_t *)) d->ptr[op[2]])(&vr[op[1]]);
+			break;	case op_func0_d:	vd[op[1]] = ((double (*)(void)) ptr[op[2]])();
+			break;	case op_func0_r:	((void (*)(uint8_t *)) ptr[op[2]])(&vr[op[1]]);
 
 			// 4 word ops
-			break;	case op_ret3_d:		for (i=0; i<3; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x3
-			break;	case op_ret3_r:		for (i=0; i<3; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
+			break;	case op_ret3_d:		for (i=0; i<3; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x3
+			break;	case op_ret3_r:		for (i=0; i<3; i++) rf->set(&return_real[i], &vr[op[i+1]]);
 			break;	case op_add_dd:		vd[op[1]] = vd[op[2]] + vd[op[3]];				// <var> = add <var/ptr/expr> <var/ptr/expr>
 			break;	case op_add_ii:		vi[op[1]] = vi[op[2]] + vi[op[3]];				// <var> = addi <var/ptr/expr> <var/ptr/expr>
 			break;	case op_sub_dd:		vd[op[1]] = vd[op[2]] - vd[op[3]];				// sub
@@ -78,82 +93,88 @@ int rlip_execute_opcode(rlip_t *d)
 
 			break;	case op_cmp_dd_eq:	vi[op[1]] = (vd[op[2]] == vd[op[3]]);				// <var> = cmp <var/ptr/expr> == <var/ptr/expr>
 			break;	case op_cmp_ii_eq:	vi[op[1]] = (vi[op[2]] == vi[op[3]]);				// <var> = cmpi <var/ptr/expr> == <var/ptr/expr>
-			break;	case op_cmp_rr_eq:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) == 0;		// <var> = cmpr <var/ptr/expr> == <var/ptr/expr>
+			break;	case op_cmp_rr_eq:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) == 0;		// <var> = cmpr <var/ptr/expr> == <var/ptr/expr>
 			break;	case op_cmp_dd_ne:	vi[op[1]] = (vd[op[2]] != vd[op[3]]);
 			break;	case op_cmp_ii_ne:	vi[op[1]] = (vi[op[2]] != vi[op[3]]);
-			break;	case op_cmp_rr_ne:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) != 0;
+			break;	case op_cmp_rr_ne:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) != 0;
 			break;	case op_cmp_dd_lt:	vi[op[1]] = (vd[op[2]] <  vd[op[3]]);
 			break;	case op_cmp_ii_lt:	vi[op[1]] = (vi[op[2]] <  vi[op[3]]);
-			break;	case op_cmp_rr_lt:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) < 0;
+			break;	case op_cmp_rr_lt:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) < 0;
 			break;	case op_cmp_dd_le:	vi[op[1]] = (vd[op[2]] <= vd[op[3]]);
 			break;	case op_cmp_ii_le:	vi[op[1]] = (vi[op[2]] <= vi[op[3]]);
-			break;	case op_cmp_rr_le:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) <= 0;
+			break;	case op_cmp_rr_le:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) <= 0;
 			break;	case op_cmp_dd_gt:	vi[op[1]] = (vd[op[2]] >  vd[op[3]]);
 			break;	case op_cmp_ii_gt:	vi[op[1]] = (vi[op[2]] >  vi[op[3]]);
-			break;	case op_cmp_rr_gt:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) > 0;
+			break;	case op_cmp_rr_gt:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) > 0;
 			break;	case op_cmp_dd_ge:	vi[op[1]] = (vd[op[2]] >= vd[op[3]]);
 			break;	case op_cmp_ii_ge:	vi[op[1]] = (vi[op[2]] >= vi[op[3]]);
-			break;	case op_cmp_rr_ge:	vi[op[1]] = d->rf.cmp(&vr[op[2]], &vr[op[3]]) >= 0;
-			break;	case op_func1_dd:	vd[op[1]] = ((double (*)(double)) d->ptr[op[2]])(vd[op[3]]);	// <var> = <func> <var/ptr/expr>
-			break;	case op_func1_di:	vd[op[1]] = ((double (*)(int64_t)) d->ptr[op[2]])(vi[op[3]]);
-			break;	case op_func1_ii:	vi[op[1]] = ((int64_t (*)(int64_t)) d->ptr[op[2]])(vi[op[3]]);
-			break;	case op_func1_id:	vi[op[1]] = ((int64_t (*)(double)) d->ptr[op[2]])(vd[op[3]]);
-			break;	case op_func1_rr:	((void (*)(uint8_t *,uint8_t *)) d->ptr[op[2]])(&vr[op[1]], &vr[op[3]]);
+			break;	case op_cmp_rr_ge:	vi[op[1]] = rf->cmp(&vr[op[2]], &vr[op[3]]) >= 0;
+			break;	case op_func1_dd:	vd[op[1]] = ((double (*)(double)) ptr[op[2]])(vd[op[3]]);	// <var> = <func> <var/ptr/expr>
+			break;	case op_func1_di:	vd[op[1]] = ((double (*)(int64_t)) ptr[op[2]])(vi[op[3]]);
+			break;	case op_func1_ii:	vi[op[1]] = ((int64_t (*)(int64_t)) ptr[op[2]])(vi[op[3]]);
+			break;	case op_func1_id:	vi[op[1]] = ((int64_t (*)(double)) ptr[op[2]])(vd[op[3]]);
+			break;	case op_func1_rr:	((void (*)(uint8_t *,uint8_t *)) ptr[op[2]])(&vr[op[1]], &vr[op[3]]);
 
 			// 5 word ops
-			break;	case op_ret4_d:		for (i=0; i<4; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x4
-			break;	case op_ret4_r:		for (i=0; i<4; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
+			break;	case op_ret4_d:		for (i=0; i<4; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x4
+			break;	case op_ret4_r:		for (i=0; i<4; i++) rf->set(&return_real[i], &vr[op[i+1]]);
 			break;	case op_aad_ddd:	vd[op[1]] = vd[op[2]] + vd[op[3]] + vd[op[4]];			// <var> = aad <var/ptr/expr> <var/ptr/expr> <var/ptr/expr>
 			break;	case op_mmul_ddd:	vd[op[1]] = vd[op[2]] * vd[op[3]] * vd[op[4]];			// mmul (two multiplications)
 			break;	case op_mad_ddd:	vd[op[1]] = vd[op[2]] * vd[op[3]] + vd[op[4]];			// mad (multiply-add)
 			break;	case op_adm_ddd:	vd[op[1]] = (vd[op[2]] + vd[op[3]]) * vd[op[4]];		// adm (multiplication of sum)
-			break;	case op_func2_ddd:	vd[op[1]] = ((double (*)(double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]]);
-			break;	case op_func2_iii:	vi[op[1]] = ((int64_t (*)(int64_t,int64_t)) d->ptr[op[2]])(vi[op[3]], vi[op[4]]);
-			break;	case op_func2_rrr:	((void (*)(uint8_t *,uint8_t *,uint8_t *)) d->ptr[op[2]])(&vr[op[1]], &vr[op[3]], &vr[op[4]]);
-			break;	case op_func2_dii:	vd[op[1]] = ((double (*)(int64_t,int64_t)) d->ptr[op[2]])(vi[op[3]], vi[op[4]]);
+			break;	case op_func2_ddd:	vd[op[1]] = ((double (*)(double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]]);
+			break;	case op_func2_iii:	vi[op[1]] = ((int64_t (*)(int64_t,int64_t)) ptr[op[2]])(vi[op[3]], vi[op[4]]);
+			break;	case op_func2_rrr:	((void (*)(uint8_t *,uint8_t *,uint8_t *)) ptr[op[2]])(&vr[op[1]], &vr[op[3]], &vr[op[4]]);
+			break;	case op_func2_dii:	vd[op[1]] = ((double (*)(int64_t,int64_t)) ptr[op[2]])(vi[op[3]], vi[op[4]]);
 
 			// 6 word ops
-			break;	case op_ret5_d:		for (i=0; i<5; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x5
-			break;	case op_ret5_r:		for (i=0; i<5; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
-			break;	case op_func3_dddd:	vd[op[1]] = ((double (*)(double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]]);
-			break;	case op_func3_dddi:	vd[op[1]] = ((double (*)(double,double,int64_t)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vi[op[5]]);
-			break;	case op_func3_rrrr:	((void (*)(uint8_t *,uint8_t *,uint8_t *,uint8_t *)) d->ptr[op[2]])(&vr[op[1]], &vr[op[3]], &vr[op[4]], &vr[op[5]]);
-			break;	case op_func3_diid:	vd[op[1]] = ((double (*)(int64_t,int64_t,double)) d->ptr[op[2]])(vi[op[3]], vi[op[4]], vd[op[5]]);
+			break;	case op_ret5_d:		for (i=0; i<5; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x5
+			break;	case op_ret5_r:		for (i=0; i<5; i++) rf->set(&return_real[i], &vr[op[i+1]]);
+			break;	case op_func3_dddd:	vd[op[1]] = ((double (*)(double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]]);
+			break;	case op_func3_dddi:	vd[op[1]] = ((double (*)(double,double,int64_t)) ptr[op[2]])(vd[op[3]], vd[op[4]], vi[op[5]]);
+			break;	case op_func3_rrrr:	((void (*)(uint8_t *,uint8_t *,uint8_t *,uint8_t *)) ptr[op[2]])(&vr[op[1]], &vr[op[3]], &vr[op[4]], &vr[op[5]]);
+			break;	case op_func3_diid:	vd[op[1]] = ((double (*)(int64_t,int64_t,double)) ptr[op[2]])(vi[op[3]], vi[op[4]], vd[op[5]]);
 
 			// 7 word ops
-			break;	case op_ret6_d:		for (i=0; i<6; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x6
-			break;	case op_ret6_r:		for (i=0; i<6; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
-			break;	case op_func4_ddddd:	vd[op[1]] = ((double (*)(double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]]);
-			break;	case op_func4_diddi:	vd[op[1]] = ((double (*)(int64_t,double,double,int64_t)) d->ptr[op[2]])(vi[op[3]], vd[op[4]], vd[op[5]], vi[op[6]]);
+			break;	case op_ret6_d:		for (i=0; i<6; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x6
+			break;	case op_ret6_r:		for (i=0; i<6; i++) rf->set(&return_real[i], &vr[op[i+1]]);
+			break;	case op_func4_ddddd:	vd[op[1]] = ((double (*)(double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]]);
+			break;	case op_func4_diddi:	vd[op[1]] = ((double (*)(int64_t,double,double,int64_t)) ptr[op[2]])(vi[op[3]], vd[op[4]], vd[op[5]], vi[op[6]]);
 
 			// 8 word ops
-			break;	case op_ret7_d:		for (i=0; i<7; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x7
-			break;	case op_ret7_r:		for (i=0; i<7; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
-			break;	case op_func5_dddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]]);
+			break;	case op_ret7_d:		for (i=0; i<7; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x7
+			break;	case op_ret7_r:		for (i=0; i<7; i++) rf->set(&return_real[i], &vr[op[i+1]]);
+			break;	case op_func5_dddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]]);
 
 			// 9 word ops
-			break;	case op_ret8_d:		for (i=0; i<8; i++) d->return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x8
-			break;	case op_ret8_r:		for (i=0; i<8; i++) d->rf.set(&d->return_real[i], &vr[op[i+1]]);
-			break;	case op_func6_ddddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]]);
+			break;	case op_ret8_d:		for (i=0; i<8; i++) return_value[i] = vd[op[i+1]];		// return <var/ptr/expr> x8
+			break;	case op_ret8_r:		for (i=0; i<8; i++) rf->set(&return_real[i], &vr[op[i+1]]);
+			break;	case op_func6_ddddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]]);
 
 			// 10 word ops
-			break;	case op_func7_dddddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]]);
+			break;	case op_func7_dddddddd:	vd[op[1]] = ((double (*)(double,double,double,double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]]);
 
 			// 11 word ops
-			break;	case op_func8_ddddddddd: vd[op[1]] = ((double (*)(double,double,double,double,double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]], vd[op[10]]);
+			break;	case op_func8_ddddddddd: vd[op[1]] = ((double (*)(double,double,double,double,double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]], vd[op[10]]);
 
 			// 12 word ops
-			break;	case op_func9_dddddddddd: vd[op[1]] = ((double (*)(double,double,double,double,double,double,double,double,double)) d->ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]], vd[op[10]], vd[op[11]]);
+			break;	case op_func9_dddddddddd: vd[op[1]] = ((double (*)(double,double,double,double,double,double,double,double,double)) ptr[op[2]])(vd[op[3]], vd[op[4]], vd[op[5]], vd[op[6]], vd[op[7]], vd[op[8]], vd[op[9]], vd[op[10]], vd[op[11]]);
 
 			break;
 			default:
-				fprintf_rl(stderr, "Invalid opcode '%d' at op[%zu]\n", op[0], (op - d->op) / sizeof(opint_t));
+				fprintf_rl(stderr, "Invalid opcode '%d' at op[%zu]\n", code, op - d->op);
 				return -2;
 		}
 
 		// Increment to next op
 		op = op_next;
-	}
 
-	return 0;
+		loop_count--;
+		if (loop_count == 0)
+		{
+			if (*exec_on == 0)
+				return 0;
+			loop_count = RLIP_KILLSWITCH_POLL_INTERVAL;
+		}
+	}
 }
